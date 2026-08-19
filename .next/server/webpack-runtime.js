7b8764e15a2850cc298a088924fdce9752bd8a4a
lucianobrito220 <luciano.brito220@gmail.com>
2026-08-19 01:13:02 -0300

    style(ux): update typography to Apple system fonts (SF Pro) for native iPhone feel

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("30cd1ddeb78c8aab")
+/******/ 		__webpack_require__.h = () => ("5f3b37830ec90c1f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5dff0ddb1fe3c052"; }
+/******/ 		__webpack_require__.h = function() { return "45c447a17ab173f4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
